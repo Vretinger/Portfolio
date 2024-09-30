b029a9a09c552abfe4c25f216b0786fc5b138b7e
Vretinger Vretinger <brozgamers@hotmail.com>
2024-09-30 23:09:09 +0000

    Add about page
    Fix website theme
    
    Optimize for different screen resolutions

--- my_portfolio/urls.py
+++ my_portfolio/urls.py
@@ -21,6 +21,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('contact_me/', include('contact_me.urls')), 
-    path('about_me/', include('about_me.urls')), 
+    path('contact_me/', include('contact_me.urls')),
+    path('about/', views.about, name='about'),
 ]
